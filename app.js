e7427f216b71a6305376b273889a84fe8de2204f
jason <jason_doan94@yahoo.com>
2020-06-29 03:20:26 -0400

    nav bar

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-//scroll to top on page refresh
-window.onbeforeunload = function() {
-  window.scrollTo(0, 0);
-};
-//for arrow click
-$(document).ready(function() {
-  $("#down").on("click", function() {
-    $("html").scrollTop(0);
-    $("html, body").animate(
-      {
-        scrollTop: $("#featuredProducts").offset().top
-      },
-      1000
-    );
-  });
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
