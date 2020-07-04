c62d143c2673d27411d1411978ae5eb7b4bc6893
jason <jason_doan94@yahoo.com>
2020-07-04 00:05:32 -0400

    slider

---
+++ app.js
@@ -0,0 +1,41 @@
+// let mainNav = document.getElementById("js-menu");
+// let navBarToggle = document.getElementById("js-navbar-toggle");
+
+// navBarToggle.addEventListener("click", function() {
+//   mainNav.classList.toggle("active");
+// });
+
+$(".post-wrapper").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: $(".next"),
+  prevArrow: $(".prev"),
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
